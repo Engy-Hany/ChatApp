8cfeb21e86b0460eead2a9407fbef57703bb6443
Engy Hany <engyhany925@gmail.com>
2024-10-22 06:01:39 +0300

    third commit

---
+++ lib/core/themes/styles.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+abstract class styles{
+  static TextStyle textStyle25 =TextStyle(
+    fontSize: 25,
+    color: Colors.black,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle textStyle18 =TextStyle(
+    fontSize: 18,
+    color: Colors.black,
+  );
+
+}
