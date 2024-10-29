26c9e9b9bd4483fe121b6af200c9176b094aa9f5
Engy Hany <engyhany925@gmail.com>
2024-10-29 12:32:44 +0300

    feature-Call

--- lib/features/call/presentation/views/call_screen.dart
+++ lib/features/call/presentation/views/call_screen.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/features/call/presentation/views/widgets/call_page.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class CallScreen extends StatelessWidget{
   @override
---
+++ lib/features/call/presentation/views/widgets/recent_call.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../core/constants/asset_images.dart';
+import '../../../../../core/themes/colors_app.dart';
+import '../../../../../core/themes/styles.dart';
+
+class RecentCall extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+      return ListView.builder(
+          shrinkWrap: true,
+          itemCount: 10,
+          itemBuilder:  (context,index){
+               return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage:  AssetImage(AssetImages.statusPic),
+                    radius: 35,
+                  ),
+                  title: Text(
+                    'Marwan Ali',
+                    style: styles.textStyle16.copyWith(
+                      color: ColorsApp.secondColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Row(
+                    children: [
+                      Icon(
+                        Icons.call_made,
+                        color: ColorsApp.primaryColor,
+                      ),
+                      Text(
+                        'Today, 12:00 PM',
+                        style: styles.textStyle16.copyWith(
+                          color: ColorsApp.thirdColor,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  trailing: IconButton(
+                    onPressed: () {  },
+                    icon: Icon(
+                      Icons.call,
+                      color: ColorsApp.secondColor,
+                    ),
+                  ),
+                );
+
+          });
+  }
+
+}
