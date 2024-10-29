1944618245a897c974b720619b7438fb3d04c210
Engy Hany <engyhany925@gmail.com>
2024-10-29 13:57:39 +0300

    feature-Profile

--- lib/features/profile/presentation/views/widgets/profile_appbar.dart
+++ lib/features/profile/presentation/views/widgets/profile_appbar.dart
@@ -2,6 +2,8 @@ import 'package:chat_app/core/themes/colors_app.dart';
 import 'package:chat_app/core/themes/styles.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../home/presentation/views/home_screen.dart';
+
 class ProfileAppbar extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
@@ -9,7 +11,9 @@ class ProfileAppbar extends StatelessWidget{
          leading: IconButton(
            icon: Icon(
              Icons.arrow_back_outlined
-           ) , onPressed: () {  },
+           ) , onPressed: () {
+             Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen()));
+         },
          ),
          title: Text(
            'Profile',
